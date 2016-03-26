66884b40bdd7f896f7fde1f2057155a92055efeb
Celrenheit <celrenheit+github@gmail.com>
2016-03-26 15:51:32 +0100

    Add missing copyright and license

--- bench_test.go
+++ bench_test.go
@@ -1,3 +1,6 @@
+// Copyright 2013 Julien Schmidt. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
 package lion
 
 import (
