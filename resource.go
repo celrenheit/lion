5feedd5fa1df7cd6d9d9de8a09f4d7ecb2535c29
Celrenheit <celrenheit+github@gmail.com>
2016-04-01 22:31:33 +0200

    Add support for HEAD, TRACE, OPTIONS, CONNECT and PATCH in resources

--- resource.go
+++ resource.go
@@ -19,6 +19,11 @@ type GetResourceMiddlewares interface {
 	GetMiddlewares() Middlewares
 }
 
+// HeadResourceMiddlewares is an interface for defining middlewares used in Resource method
+type HeadResourceMiddlewares interface {
+	HeadMiddlewares() Middlewares
+}
+
 // PostResourceMiddlewares is an interface for defining middlewares used in Resource method
 type PostResourceMiddlewares interface {
 	PostMiddlewares() Middlewares
@@ -34,9 +39,34 @@ type DeleteResourceMiddlewares interface {
 	DeleteMiddlewares() Middlewares
 }
 
+// TraceResourceMiddlewares is an interface for defining middlewares used in Resource method
+type TraceResourceMiddlewares interface {
+	TraceMiddlewares() Middlewares
+}
+
+// OptionsResourceMiddlewares is an interface for defining middlewares used in Resource method
+type OptionsResourceMiddlewares interface {
+	OptionsMiddlewares() Middlewares
+}
+
+// ConnectResourceMiddlewares is an interface for defining middlewares used in Resource method
+type ConnectResourceMiddlewares interface {
+	ConnectMiddlewares() Middlewares
+}
+
+// PatchResourceMiddlewares is an interface for defining middlewares used in Resource method
+type PatchResourceMiddlewares interface {
+	PatchMiddlewares() Middlewares
+}
+
 // GetResource is an interface for defining a HandlerFunc used in Resource method
 type GetResource interface {
 	Get(c context.Context, w http.ResponseWriter, r *http.Request)
+}
+
+// HeadResource is an interface for defining a HandlerFunc used in Resource method
+type HeadResource interface {
+	Head(c context.Context, w http.ResponseWriter, r *http.Request)
 }
 
 // PostResource is an interface for defining a HandlerFunc used in Resource method
@@ -54,6 +84,26 @@ type DeleteResource interface {
 	Delete(c context.Context, w http.ResponseWriter, r *http.Request)
 }
 
+// TraceResource is an interface for defining a HandlerFunc used in Resource method
+type TraceResource interface {
+	Trace(c context.Context, w http.ResponseWriter, r *http.Request)
+}
+
+// OptionsResource is an interface for defining a HandlerFunc used in Resource method
+type OptionsResource interface {
+	Options(c context.Context, w http.ResponseWriter, r *http.Request)
+}
+
+// ConnectResource is an interface for defining a HandlerFunc used in Resource method
+type ConnectResource interface {
+	Connect(c context.Context, w http.ResponseWriter, r *http.Request)
+}
+
+// PatchResource is an interface for defining a HandlerFunc used in Resource method
+type PatchResource interface {
+	Patch(c context.Context, w http.ResponseWriter, r *http.Request)
+}
+
 // Resource registers a Resource with the corresponding pattern
 func (r *Router) Resource(pattern string, resource Resource) {
 	sub := r.Group(pattern)
@@ -64,6 +114,7 @@ func (r *Router) Resource(pattern string, resource Resource) {
 		}
 	}
 
+	// GET
 	if res, ok := resource.(GetResource); ok {
 		s := sub.Group("/")
 		if mw, ok := resource.(GetResourceMiddlewares); ok {
@@ -72,6 +123,16 @@ func (r *Router) Resource(pattern string, resource Resource) {
 		s.GetFunc("/", res.Get)
 	}
 
+	// HEAD
+	if res, ok := resource.(HeadResource); ok {
+		s := sub.Group("/")
+		if mw, ok := resource.(HeadResourceMiddlewares); ok {
+			s.Use(mw.HeadMiddlewares()...)
+		}
+		s.HeadFunc("/", res.Head)
+	}
+
+	// POST
 	if res, ok := resource.(PostResource); ok {
 		s := sub.Group("/")
 		if mw, ok := resource.(PostResourceMiddlewares); ok {
@@ -80,6 +141,7 @@ func (r *Router) Resource(pattern string, resource Resource) {
 		s.PostFunc("/", res.Post)
 	}
 
+	// PUT
 	if res, ok := resource.(PutResource); ok {
 		s := sub.Group("/")
 		if mw, ok := resource.(PutResourceMiddlewares); ok {
@@ -88,11 +150,48 @@ func (r *Router) Resource(pattern string, resource Resource) {
 		s.PutFunc("/", res.Put)
 	}
 
+	// DELETE
 	if res, ok := resource.(DeleteResource); ok {
 		s := sub.Group("/")
 		if mw, ok := resource.(DeleteResourceMiddlewares); ok {
 			s.Use(mw.DeleteMiddlewares()...)
 		}
 		s.DeleteFunc("/", res.Delete)
+	}
+
+	// TRACE
+	if res, ok := resource.(TraceResource); ok {
+		s := sub.Group("/")
+		if mw, ok := resource.(TraceResourceMiddlewares); ok {
+			s.Use(mw.TraceMiddlewares()...)
+		}
+		s.TraceFunc("/", res.Trace)
+	}
+
+	// OPTIONS
+	if res, ok := resource.(OptionsResource); ok {
+		s := sub.Group("/")
+		if mw, ok := resource.(OptionsResourceMiddlewares); ok {
+			s.Use(mw.OptionsMiddlewares()...)
+		}
+		s.OptionsFunc("/", res.Options)
+	}
+
+	// CONNECT
+	if res, ok := resource.(ConnectResource); ok {
+		s := sub.Group("/")
+		if mw, ok := resource.(ConnectResourceMiddlewares); ok {
+			s.Use(mw.ConnectMiddlewares()...)
+		}
+		s.ConnectFunc("/", res.Connect)
+	}
+
+	// PATCH
+	if res, ok := resource.(PatchResource); ok {
+		s := sub.Group("/")
+		if mw, ok := resource.(PatchResourceMiddlewares); ok {
+			s.Use(mw.PatchMiddlewares()...)
+		}
+		s.PatchFunc("/", res.Patch)
 	}
 }
