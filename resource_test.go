50c811d11dedc412e49297f3266247fb08776222
Celrenheit <celrenheit+github@gmail.com>
2016-04-02 12:23:33 +0200

    Add tests for missing http methods in resources

--- resource_test.go
+++ resource_test.go
@@ -1,6 +1,7 @@
 package lion
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,58 +13,48 @@ type testResource struct{}
 
 func (tr testResource) Uses() Middlewares { return Middlewares{} }
 
-func (tr testResource) GetMiddlewares() Middlewares {
-	return Middlewares{MiddlewareFunc(func(next Handler) Handler {
-		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("foo", "Get")
-			next.ServeHTTPC(c, w, r)
-		})
-	})}
-}
-
-func (tr testResource) PostMiddlewares() Middlewares {
-	return Middlewares{MiddlewareFunc(func(next Handler) Handler {
-		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("foo", "Post")
-			next.ServeHTTPC(c, w, r)
-		})
-	})}
-}
-func (tr testResource) PutMiddlewares() Middlewares {
-	return Middlewares{MiddlewareFunc(func(next Handler) Handler {
-		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("foo", "Put")
-			next.ServeHTTPC(c, w, r)
-		})
-	})}
-}
-func (tr testResource) DeleteMiddlewares() Middlewares {
-	return Middlewares{MiddlewareFunc(func(next Handler) Handler {
-		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("foo", "Delete")
-			next.ServeHTTPC(c, w, r)
-		})
-	})}
-}
+func (tr testResource) GetMiddlewares() Middlewares     { return Middlewares{newTestResMW("Get")} }
+func (tr testResource) HeadMiddlewares() Middlewares    { return Middlewares{newTestResMW("Head")} }
+func (tr testResource) PostMiddlewares() Middlewares    { return Middlewares{newTestResMW("Post")} }
+func (tr testResource) PutMiddlewares() Middlewares     { return Middlewares{newTestResMW("Put")} }
+func (tr testResource) DeleteMiddlewares() Middlewares  { return Middlewares{newTestResMW("Delete")} }
+func (tr testResource) TraceMiddlewares() Middlewares   { return Middlewares{newTestResMW("Trace")} }
+func (tr testResource) OptionsMiddlewares() Middlewares { return Middlewares{newTestResMW("Options")} }
+func (tr testResource) ConnectMiddlewares() Middlewares { return Middlewares{newTestResMW("Connect")} }
+func (tr testResource) PatchMiddlewares() Middlewares   { return Middlewares{newTestResMW("Patch")} }
 
 func (tr testResource) Get(c context.Context, w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Get"))
+	fmt.Fprintf(w, "Get")
+}
+func (tr testResource) Head(c context.Context, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Head")
 }
 func (tr testResource) Post(c context.Context, w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Post"))
+	fmt.Fprintf(w, "Post")
 }
 func (tr testResource) Put(c context.Context, w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Put"))
+	fmt.Fprintf(w, "Put")
 }
 func (tr testResource) Delete(c context.Context, w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Delete"))
+	fmt.Fprintf(w, "Delete")
+}
+func (tr testResource) Trace(c context.Context, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Trace")
+}
+func (tr testResource) Options(c context.Context, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Options")
+}
+func (tr testResource) Connect(c context.Context, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Connect")
+}
+func (tr testResource) Patch(c context.Context, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Patch")
 }
 
 func TestResources(t *testing.T) {
-	methods := []string{"GET", "POST", "PUT", "DELETE"}
-	expected := []string{"Get", "Post", "Put", "Delete"}
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "OPTIONS", "CONNECT", "PATCH"}
+	expected := []string{"Get", "Head", "Post", "Put", "Delete", "Trace", "Options", "Connect", "Patch"}
 	tr := testResource{}
-	// hfuncs := []HandlerFunc{tr.Get, tr.Post, tr.Put, tr.Delete}
 
 	r := New()
 	r.Resource("/testpath", tr)
@@ -82,4 +73,13 @@ func TestResources(t *testing.T) {
 			t.Errorf("[Resource] Expected header %s but got %s for http method %s", expected[i], w.Header().Get("foo"), methods[i])
 		}
 	}
+}
+
+func newTestResMW(header string) Middleware {
+	return MiddlewareFunc(func(next Handler) Handler {
+		return HandlerFunc(func(c context.Context, w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("foo", header)
+			next.ServeHTTPC(c, w, r)
+		})
+	})
 }
